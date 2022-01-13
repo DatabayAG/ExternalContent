58e9321260011d13ec777a7c79875ee1b3abebcf
Fred Neumann <fred.neumann@fau.de>
2022-01-13 10:36:29 +0100

    fixed plugin.php

--- plugin.php
+++ plugin.php
@@ -15,12 +15,11 @@
 $id = "xxco";
  
 // code version; must be changed for all code changes
-define('xxco_version', '1.7.1');
-$version = xxco_version;
+$version = '1.7.1';
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.4.0";
+$ilias_min_version = "7.4";
 $ilias_max_version = "7.999";
  
 // optional, but useful: Add one or more responsible persons and a contact email
